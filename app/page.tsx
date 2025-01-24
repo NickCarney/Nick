bc1206aaf307ed052d9fd7e0920fd82d4548e10a
Nick Carney <npcarney@uvm.edu>
2025-01-24 16:57:17 -0500

    lists start

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,26 @@ export default function Home() {
         <p className="geist-sans text-[24px] text-center">
           Welcome to Nick Carney's personal website
         </p>
+        <div className="flex flex-row pt-[5%]">
+          <div className="w-full text-center">
+            <p>Personal Interests:</p>
+            <ol className="">
+              <li>Nature</li>
+              <li>Sports</li>
+              <li>Music</li>
+              <li>Learning</li>
+            </ol>
+          </div>
+          <div className="w-full text-center">
+            <p>Academic Interests:</p>
+            <ol className="">
+              <li>Programming</li>
+              <li>Blockchain</li>
+              <li>Data Analytics</li>
+              <li>WebDev</li>
+            </ol>
+          </div>
+        </div>
       </main>
     </div>
   );
